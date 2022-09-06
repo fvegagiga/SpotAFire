7b02f367fd83a6950e32befc32922cbe03c2dd55
Fernando Vega <fernando.vega@cabify.com>
2022-09-06 11:23:06 +0200

    Create the Spot model

---
+++ SpotAFireFeed/SpotAFireFeed/Spot.swift
@@ -0,0 +1,11 @@
+// Copyright @ 2022 Fernando Vega. All rights reserved.
+
+import Foundation
+
+struct Spot {
+    let id: String
+    let author: String
+    let description: String?
+    let likes: Int
+    let image: URL
+}
